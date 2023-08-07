e6e9076bf1059a775e3400e6054f629e9ffbc699
Lussati Santos <lussatisantos10@gmail.com>
2023-08-07 13:14:43 +0100

    reorganizado
    Reorganizado a pasta do curso de php, entre os exercicios da aula e os desafios propostos

--- desafios/d001/nova.php
+++ desafios/d001/nova.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado</title>
-    <link rel="stylesheet" href="../ex004/style.css">
+    <link rel="stylesheet" href="../../exercicios/ex004/style.css">
 </head>
 <body>
     <main>
