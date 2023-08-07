05637d98b9b833f2f3cc42b540767ec897fc0d37
Lussati Santos <lussatisantos10@gmail.com>
2023-08-07 12:46:15 +0100

    finish

--- ex005/nova.php
+++ ex005/nova.php
@@ -4,15 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado</title>
+    <link rel="stylesheet" href="../ex004/style.css">
 </head>
 <body>
     <?php 
-        $num = $_GET["numero"];
+        $num = $_POST["numero"];
         $snum = $num + 1;
         $anum = $num - 1;
+        echo "<header>
+        <h1> Resultado</h1>
+        </header>";
         print("O valor digitado é $num");
-        print("O sucessor de $num é $snum");
-        print("O antecessor de $num é $anum");
+        print("<p> O sucessor é $snum </p>");
+        print("O antecessor é $anum");
+
     ?>
 </body>
 </html>
