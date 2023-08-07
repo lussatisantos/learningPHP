8c67a08d99c9819fd31f093c553245b62c12ce22
Lussati Santos <lussatisantos10@gmail.com>
2023-08-07 13:35:27 +0100

    d002

---
+++ desafios/d002/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 002</title>
+    <link rel="stylesheet" href="../../exercicios/ex004/style.css">
+</head>
+<body>
+    <main>
+
+    </main>
+</body>
+</html>
