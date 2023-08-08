586789f1a70ef196155fe561672f3b02063ecac7
Lussati Santos <lussatisantos10@gmail.com>
2023-08-08 14:25:01 +0100

    cotacao actualizada

--- desafios/d004/teste.php
+++ desafios/d004/teste.php
@@ -1,6 +1,10 @@
 <pre>
 <?php 
-    $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'08-01-2023\'&@dataFinalCotacao=\'08-08-2023\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+
+    $inicio = date("m-d-Y", strtotime("-7 days"));
+    $fim = date("m-d-Y");
+
+    $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''. $inicio .'\'&@dataFinalCotacao=\''. $fim .'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
 
     $dados = json_decode(file_get_contents($url), true);
 
