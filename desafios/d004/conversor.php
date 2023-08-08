22bac172a86c61720831212fda7740f1517b7bbf
Lussati Santos <lussatisantos10@gmail.com>
2023-08-08 15:05:57 +0100

    Update conversor.php

--- desafios/d004/conversor.php
+++ desafios/d004/conversor.php
@@ -18,8 +18,15 @@
                 $fim = date("m-d-Y");
                 $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo      (dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\''. $inicio .'\'&@dataFinalCotacao=\''. $fim .'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
                 $dados = json_decode(file_get_contents($url), true);
+
                 $cotacao = $dados["value"][0]["cotacaoCompra"];
                 
+                $conversao = $moeda + $cotacao;
+                $padrao = numfmt_create("pt_AO", NumberFormatter::CURRENCY);
+
+                echo "Voce pretende converter " . numfmt_format_currency($padrao, $moeda, "AOA") . " em Dolar Americano";
+
+                echo "<br> Por sua vez, " . numfmt_format_currency($padrao, $moeda, "AOA") . " equivalem a " . numfmt_format_currency($padrao, $conversao, "USD");
             ?>
             <p></p>
 <button onclick="javascript:window.location.href='index.html'">Voltar a converter</button>
