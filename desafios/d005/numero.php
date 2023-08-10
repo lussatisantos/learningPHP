38d28569cca31ca7c61b45fa668eb7c3a374995e
Lussati Santos <lussatisantos10@gmail.com>
2023-08-11 00:15:00 +0100

    numeros reais em php

--- desafios/d005/numero.php
+++ desafios/d005/numero.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Analisador</title>
+    <link rel="stylesheet" href="../../exercicios/ex004/style.css">
 </head>
 <body>
+    <h1>Resulltado da analise</h1>
     <main>
         <?php 
             $num = $_POST["n"];
