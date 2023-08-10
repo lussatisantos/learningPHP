5cde261f6f4776fd1f0c822f987f003dae4fc751
Lussati Santos <lussatisantos10@gmail.com>
2023-08-11 00:11:38 +0100

    php

---
+++ desafios/d005/numero.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Analisador</title>
+</head>
+<body>
+    <main>
+        <?php 
+            $num = $_POST["n"];
+            echo "$num";
+        ?>
+    </main>
+</body>
+</html>
