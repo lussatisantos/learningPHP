b033ea48b424db0ad590ff77d99a3b3d1d4c8ba9
Lussati Santos <lussatisantos10@gmail.com>
2023-08-12 15:29:33 +0100

    Update superglobais.php

--- exercicios/ex005/superglobais.php
+++ exercicios/ex005/superglobais.php
@@ -32,9 +32,12 @@
             
                 echo "<h1>Superglobal ENV</h1>";
                 var_dump($_ENV);
-                foreach (getenv() as $c => $v){
-                    echo "<br> $c - $v";
-                }
+               // foreach (getenv() as $c => $v){
+                //    echo "<br> $c - $v";
+                //}
+
+                echo "<h1>Superglobal SERVER</h1>";
+                var_dump($_SERVER)
             ?>
         </pre>
     </main>
