6c9ef321b2be3b80cbf80fb04164e58d3ff87040
Lussati Santos <lussatisantos10@gmail.com>
2023-08-12 15:10:48 +0100

    Update superglobais.php

--- exercicios/ex005/superglobais.php
+++ exercicios/ex005/superglobais.php
@@ -14,7 +14,10 @@
                 var_dump($_GET);
 
                 echo "<h1>Superglobais POST</h1>";
-                var_dump($_POST)
+                var_dump($_POST);
+
+                echo "<h1>Superglobal REQUEST</h1>";
+                var_dump($_REQUEST)
             ?>
         </pre>
     </main>
