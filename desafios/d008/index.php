9d5dc951c7d525a936d0bb05aae98b8061895193
Lussati Santos <lussatisantos10@gmail.com>
2023-08-13 14:24:05 +0100

    Create index.php

---
+++ desafios/d008/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+    <link rel="stylesheet" href="../../exercicios/ex005/style.css">
+</head>
+<body>
+    <main>
+        <h1>Informe um numero</h1>
+        <form action="" method="post">
+            <label for="num">Informe um numero</label>
+            <input type="number" name="num" id="idnum">
+            <input type="submit" value="Calcular Raizes">
+        </form>
+    </main>
+</body>
+</html>
