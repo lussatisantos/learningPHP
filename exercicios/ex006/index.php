88fc12912fab64d0a9a29c86f847816a11e24c14
Lussati Santos <lussatisantos10@gmail.com>
2023-08-13 12:58:43 +0100

    Update index.php

--- exercicios/ex006/index.php
+++ exercicios/ex006/index.php
@@ -7,12 +7,16 @@
     <link rel="stylesheet" href="../ex005/style.css">
 </head>
 <body>
+    <?php 
+        $valor1 = $_GET['v1'] ?? 0;
+        $valor2 = $_GET['v2'] ?? 0;
+    ?>
     <main>
-        <form action="" method="get">
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="v1">Valor 1</label>
-            <input type="number" name="v1" id="idv1">
+            <input type="number" name="v1" id="idv1" value="<?=$valor1?>">
             <label for="v2">Valor 2</label>
-            <input type="number" name="v2" id="idv2">
+            <input type="number" name="v2" id="idv2" value="<?=$valor2?>">
             <input type="submit" value="Calcular">
         </form>
     </main>
