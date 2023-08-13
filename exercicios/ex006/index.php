10aadf0a6e1a6a8e7bf8b9669e46290f5701e9e0
Lussati Santos <lussatisantos10@gmail.com>
2023-08-13 12:50:06 +0100

    Create index.php

---
+++ exercicios/ex006/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio PHP</title>
+    <link rel="stylesheet" href="../ex005/style.css">
+</head>
+<body>
+    <?php 
+    
+    ?>
+</body>
+</html>
