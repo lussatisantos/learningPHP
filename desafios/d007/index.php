fbc1400b3808535afcb4f51bac74fe386cb09d8f
Lussati Santos <lussatisantos10@gmail.com>
2023-08-13 13:52:22 +0100

    Create index.php

---
+++ desafios/d007/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio PHP</title>
+    <link rel="stylesheet" href="../../exercicios/ex005/style.css">
+</head>
+<body>
+    <main>
+        <h1>Informe seu salario</h1>
+        <form action="">
+            <label for="sal">Salario AOA</label>
+            <input type="number" name="" id="" step="0.01">
+        </form>
+    </main>
+</body>
+</html>
