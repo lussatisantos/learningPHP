6c8725429191571eb4be388c931448eb5c4b9d35
Lussati Santos <lussatisantos10@gmail.com>
2023-08-13 14:29:25 +0100

    Update index.php

--- desafios/d008/index.php
+++ desafios/d008/index.php
@@ -7,13 +7,26 @@
     <link rel="stylesheet" href="../../exercicios/ex005/style.css">
 </head>
 <body>
+    <?php 
+        $num = $_POST['num'];
+    ?>
     <main>
         <h1>Informe um numero</h1>
         <form action="" method="post">
             <label for="num">Informe um numero</label>
-            <input type="number" name="num" id="idnum">
+            <input type="number" name="num" id="idnum" value="<?=$num?>">
             <input type="submit" value="Calcular Raizes">
         </form>
     </main>
+    <?php 
+        $raizq = sqrt($num);
+        $raizcubico = $num**(1/3);
+    ?>
+    <section id="resultado">
+        <h2>Resultado final</h2>
+        <?php 
+            
+        ?>
+    </section>
 </body>
 </html>
