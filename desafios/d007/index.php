81a688d20cc6bdb58878dc79e909cf3a477cea7d
Lussati Santos <lussatisantos10@gmail.com>
2023-08-13 14:03:54 +0100

    Update index.php

--- desafios/d007/index.php
+++ desafios/d007/index.php
@@ -7,12 +7,21 @@
     <link rel="stylesheet" href="../../exercicios/ex005/style.css">
 </head>
 <body>
+    <?php 
+        $sal = $_POST["sal"] ?? 40000;
+    ?>
     <main>
         <h1>Informe seu salario</h1>
-        <form action="">
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
             <label for="sal">Salario AOA</label>
-            <input type="number" name="" id="" step="0.01">
+            <input type="number" name="sal" id="idsal" step="0.01" value="<?=$sal?>">
+            <p>Consideramos o salario minimo de <strong>40.000,00</strong></p>
+            <input type="submit" value="calcular">
         </form>
     </main>
+    <?php 
+        $tot_sal = intdiv($sal, 40000);
+        echo "$tot_sal";
+    ?>
 </body>
 </html>
