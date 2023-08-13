d743971d1e02176220f6f179e349720b118153dc
Lussati Santos <lussatisantos10@gmail.com>
2023-08-13 13:03:47 +0100

    Update index.php

--- exercicios/ex006/index.php
+++ exercicios/ex006/index.php
@@ -7,18 +7,25 @@
     <link rel="stylesheet" href="../ex005/style.css">
 </head>
 <body>
-    <?php 
+   
+    <?php
+    // Capturando os dados do formulario Retroalimentado 
         $valor1 = $_GET['v1'] ?? 0;
         $valor2 = $_GET['v2'] ?? 0;
     ?>
     <main>
+        <h1>Somador de valores</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
             <label for="v1">Valor 1</label>
             <input type="number" name="v1" id="idv1" value="<?=$valor1?>">
             <label for="v2">Valor 2</label>
             <input type="number" name="v2" id="idv2" value="<?=$valor2?>">
-            <input type="submit" value="Calcular">
+            <input type="submit" value="Somar">
         </form>
     </main>
+
+    <section id="resultado">
+        <h2>Resultado da soma</h2>
+    </section>
 </body>
 </html>
